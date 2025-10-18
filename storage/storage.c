ba0ba1d24397ae87a21a3bb37457c3276a4b5e1b
Claire Wang <diamondseaturtle@gmail.com>
2025-10-18 16:30:01 -0700

    rough load

--- storage/storage.c
+++ storage/storage.c
@@ -42,21 +42,25 @@ typedef struct recipe {
 } recipe;
 
 void myerror(const char* name, const char* fmt, ...) {
+    fprintf(stderr, "[ERROR] %s: ", name);
+
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "in %s: ", name);
     vfprintf(stderr, fmt, args); 
     va_end(args);
+
     fprintf(stderr, "\n");
 }
 
 void debug(const char* name, const char* fmt, ...) {
     if (DEBUG) {
+        printf("[DEBUG] %s: ", name);
+
         va_list args; 
         va_start(args, fmt);
-        printf("in %s: ", name);
         vfprintf(stdout, fmt, args); 
         va_end(args);
+
         printf("\n");
     }
 }
@@ -80,6 +84,29 @@ FILE* read_base(char* filename) {
     return file;
 }
 
+void line_base(FILE* file, char line[][MAX_DISPLAY], int len) {
+    char buf[MAX_LINE];
+    debug(__func__, "tokens %d", len);
+    // i dneed to be consistent with fgets and fscanf lol
+    if (!fgets(buf, MAX_LINE, file)) {
+        myerror(__func__, "cannot get line");
+    }
+    fgets(buf, MAX_LINE, file); // TODO: fix
+    buf[strlen(buf) - 1] = '\0';
+    debug(__func__, "buf %s", buf);
+
+    const char* delim = ",";
+    char* tok = strtok(buf, delim);
+    for (int i = 0; i < len; i++) {
+        strncpy(line[i], tok, MAX_DISPLAY - 1);
+        line[i][MAX_DISPLAY - 1] = '\0';
+
+        tok = strtok(NULL, delim);
+
+        debug(__func__, "line i %d, %s", i, line[i]);
+    }
+}
+
 bool load_pantry(pantry* pantry) {
     FILE* file = read_base(NUTRITION_DB); 
     if (!file) {
@@ -93,9 +120,22 @@ bool load_pantry(pantry* pantry) {
     }
 
     pantry->capacity = pantry->size == 0 ? 10 : pantry->size * 2; 
-    pantry->items = malloc(sizeof(item) * pantry->capacity);
-    
     debug(__func__, "capacity: %d, size: %d", pantry->capacity, pantry->size);
+
+    pantry->items = malloc(sizeof(item) * pantry->capacity);
+    char line[2][MAX_DISPLAY];
+    for (int i = 0; i < pantry->size; i++) {
+        line_base(file, line, 2);
+
+        item it = pantry->items[i];
+        strncpy(it.name, line[0], MAX_DISPLAY - 1);
+        it.name[MAX_DISPLAY - 1] = '\0';
+
+        it.calories = strtof(line[1], NULL);
+
+        debug(__func__, "item %s, cal %f", it.name, it.calories);
+    }
+
 
     fclose(file);
     return true;
